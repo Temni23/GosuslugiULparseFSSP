a4b61b036712ac0124299ffbbae69f3d476615e0
Artem Klimov <temni23@yandex.ru>
2023-10-30 22:06:46 +0700

    Добавлены функции для фильтрации входящих и чтения нужных новостей

--- functions.py
+++ functions.py
@@ -7,8 +7,23 @@ import requests
 from settings import URL
 
 
-def check_feeds(data: List[dict]):
+def get_incoming_document(doc_id: str, headers):
+    document_url = URL+doc_id
+    incomming_document_request = requests.get(url=document_url, headers=headers)
+    if incomming_document_request.status_code != 200:
+        raise Exception(f'Документ {doc_id} не загружен, ответ сервера '
+                        f'{incomming_document_request.status_code}')
+    incomming_document_json = incomming_document_request.json()
     pass
+
+
+def check_feeds(data: List[dict], headers: dict):
+    for element in data:
+        sender_name = element.get('title')
+        document_name = element.get('subTitle')
+        if sender_name.lower() == 'фссп россии' and 'постановление' in document_name.lower():
+            feed_id = str(element.get('id'))
+            get_incoming_document(feed_id, headers)
 
 
 def get_feeds(url_feed, cookie, date_end_check, last_feed_date='',
@@ -20,11 +35,13 @@ def get_feeds(url_feed, cookie, date_end_check, last_feed_date='',
     url = url_feed + f'?types={type_feed}&lastFeedDate={last_feed_date}'
 
     feed_request = requests.get(url=url, headers=headers)
+    if feed_request.status_code != 200:
+        raise Exception('Код не 200')
     feeds = feed_request.json().get('items')
     for i in feeds:
         print(i)
-    check_feeds(feeds)
-    last_feed_in_json = feeds[19].get('date')
+    check_feeds(feeds, headers)
+    last_feed_in_json = feeds.pop().get('date')
     more_feeds = feed_request.json().get('hasMore')
     if more_feeds and last_feed_in_json > date_end_check:
         last_feed_in_json = last_feed_in_json[:-5] + '%2B0300'
