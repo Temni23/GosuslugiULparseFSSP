331bd69145bd9de41190872962a367a5de52766e
Artem Klimov <temni23@yandex.ru>
2024-08-12 16:41:32 +0700

    Косметические исправления

--- beep.py
+++ beep.py
@@ -4,8 +4,6 @@ import os
 
 def play_sound():
     pygame.mixer.init()
-    pygame.mixer.music.load(
-        "/home/pytem/Загрузки/zvuk_-monety.mp3")  # Укажи путь к звуковому файлу
+    pygame.mixer.music.load('sounds/win31.mp3')  # Укажи путь к звуковому файлу
     pygame.mixer.music.play()
-    pygame.time.delay(
-        3000)  # Подожди 5 секунд, чтобы музыка проиграла полностью
+    pygame.time.delay(3000)
