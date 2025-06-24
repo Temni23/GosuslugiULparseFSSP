9c7404e6f675f4de1c710e4fdefad7ecd1abcc98
Artem Klimov <temni23@yandex.ru>
2025-06-24 13:47:54 +0700

    сохранение изменений до рефакторинга архитектуры

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ if __name__ == '__main__':
         print(f'Найдены уведомления по заданным параметрам, '
               f'всего {len(targets_feeds)}. Сейчас загружу документы')
         incoming_docs = get_incoming_document(targets_feeds, headers)
-        # save_incoming_vip_to_excel(incoming_docs, EXCEL_FILE_PATH)
         save_messages_to_excel(incoming_docs, inn, EXCEL_MESSAGES_FILE_PATH)
         print('Загрузка закончена')
         # Ищем и сохраняем информацию об электронных судебных приказах
